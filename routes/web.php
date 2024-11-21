642c35b52527a85fc4dd6fc9a3a671b5dc025c0d
nafimbrk <nafimubarok2005@gmail.com>
2024-11-21 14:54:06 +0700

    blog laravel

---
+++ routes/web.php
@@ -0,0 +1,56 @@
+<?php
+
+use App\Http\Controllers\AdminCategoryController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardPostController;
+use Illuminate\Support\Facades\Route;
+use App\Models\Category;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+
+Route::get('/', function () {
+    return view('home', [
+        'title' => 'Home',
+        'active' => 'home'
+    ]);
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        'title' => 'About',
+        'active' => 'about',
+        'name' => 'Nafi Mubarok',
+        'email' => 'nafimubarok@gmail.com',
+        'image' => 'dino.jpg'
+    ]);
+});
+
+
+Route::get('/posts', [PostController::class, 'index']);
+Route::get('/posts/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories', function() {
+    return view('categories', [
+        'title' => 'Post Categories',
+        'active' =>'categories',
+        'categories' => Category::all()
+    ]);
+});
+
+
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout'])->middleware('auth');
+
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', function() {
+    return view('dashboard.index');
+})->middleware('auth');
+
+Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('admin');
